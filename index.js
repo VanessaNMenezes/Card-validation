d6da651a23b18d4684854d6de928dccd1d25aa55
vanessa <vanessa_menezes_18@hotmail.com>
2023-02-11 14:59:43 -0300

    Atualizando melhorias no código

---
+++ index.js
@@ -0,0 +1,40 @@
+import validator from './validator.js';
+
+const nameCardInput = document.querySelector('#cardHolderNamer');
+const cardNumberInput = document.querySelector('#cardNumber');
+const expirationDateInput = document.querySelector('#expirationDate');
+const validationButton = document.querySelector('.validation-Button');
+const spanClose = document.querySelector('#close');
+
+validationButton.addEventListener("click" , validateCardNumber); // vai configurar uma FUNÇÃO (validateCardNumber) que será chamada sempre que o BOTÃO DE VALIDAÇÃO for clicado.
+
+function validateCardNumber() { // Função é uma ação executada assim que é chamada OU decorrência de algum evento. Pode receber paramêtros e retornar um resultado.
+
+  const modalContentElement = document.getElementById('modal-content');
+  const modalElement = document.getElementById('modal');
+  modalElement.classList.add('show-modal'); // Criando as variáveis do Pop-Up.
+
+  const identifyFullName = nameCardInput.value;
+  const identifyCardNumber = cardNumberInput.value;
+  const identifyExpirationDate = expirationDateInput.value; //value, serve para obter o valor do campo.
+
+  const creditCardNumberIsValid = validator.isValid(identifyCardNumber); // para validar um usuário, essa função de validação é usada. A validação é um método para autenticar o usuário (aqui estou validando o número do cartão de crédito).
+
+  if (creditCardNumberIsValid === true) {
+    modalContentElement.innerHTML = ` Prezado(a), ${identifyFullName}, PARABÉNS! SEU CARTÃO FOI VALIDADO COM  SUCESSO! &#x2705 <br> ${validator.maskify(identifyCardNumber)} - Data de validade: ${identifyExpirationDate} A bandeira do seu cartão é `
+
+  } else {
+    modalContentElement.innerHTML = ` Prezado(a), ${identifyFullName}, OCORREU UM ERRO &#x274C <br> POR FAVOR, INSIRA UM NÚMERO DE CARTÃO VÁLIDO ${validator.maskify(identifyCardNumber)}`
+    
+  } if (creditCardNumberIsValid === "") {
+    modalContentElement.innerHTML = ` Prezado(a), ${identifyFullName}, POR FAVOR, INSIRA O NÚMERO DO SEU CARTÃO! &#x26A0`
+  }
+}
+
+spanClose.addEventListener("click", hideModal); // vai configurar uma FUNÇÃO (hideModal) que será chamada sempre que o BOTÃO DE FECHAR for clicado.
+
+
+function hideModal() {
+  const modalElement = document.getElementById('modal'); 
+  modalElement.classList.remove('show-modal'); // vai remover a apresentação do pop-up na tela.
+}
--- src/index.js
+++ src/index.js
@@ -3,25 +3,26 @@ import validator from './validator.js';
 const nameCardInput = document.querySelector('#cardHolderNamer');
 const cardNumberInput = document.querySelector('#cardNumber');
 const expirationDateInput = document.querySelector('#expirationDate');
-const validationButton = document.querySelector('.validation-Button');
+const buttonToValidate = document.querySelector('#validationButton');
 const finalValidationMessage = document.querySelector('#validationMessage');
 
-validationButton.addEventListener("click" , validateCardNumber); // vai configurar uma FUNÇÃO (validateCardNumber) que será chamada sempre que o BOTÃO DE VALIDAÇÃO for clicado.
+buttonToValidate.addEventListener("click" , validateCardNumber); 
 
-function validateCardNumber() { // Função é uma ação executada assim que é chamada OU decorrência de algum evento. Pode receber paramêtros e retornar um resultado.
+function validateCardNumber() { 
 
   const identifyFullName = nameCardInput.value;
   const identifyCardNumber = cardNumberInput.value;
-  const identifyExpirationDate = expirationDateInput.value; //value, serve para obter o valor do campo.
+  const identifyExpirationDate = expirationDateInput.value; 
 
-  const creditCardNumberisValid = validator.isValid(identifyCardNumber); // para validar um usuário, essa função de validação é usada. A validação é um método para autenticar o usuário (aqui estou validando o número do cartão de crédito).
+  const creditCardNumberisValid = validator.isValid(identifyCardNumber);
 
   if (creditCardNumberisValid === true) {
     finalValidationMessage.innerHTML = ` Prezado(a) ${identifyFullName}, PARABÉNS!!! SEU CARTÃO FOI VALIDADO COM  SUCESSO! &#x2705 <br> ${validator.maskify(identifyCardNumber)} - Data de validade: ${identifyExpirationDate} `
-  } 
-  else {
+    
+  } else {
     finalValidationMessage.innerHTML = ` Prezado(a) ${identifyFullName}, <strong> OCORREU UM ERRO </strong> &#x274C <br> POR FAVOR, INSIRA UM NÚMERO DE CARTÃO <strong> VÁLIDO. </strong> `
   }
+
   if (identifyCardNumber === "") {
     finalValidationMessage.innerHTML = ` Prezado(a) ${identifyFullName}, <strong> POR FAVOR, INSIRA O NÚMERO DO SEU CARTÃO! </strong>⚠️`
   }
